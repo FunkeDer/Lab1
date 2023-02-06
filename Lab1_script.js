a8b92a7d564dc53f2cc116fceffbb42bfd70665b
FunkeDer <jafter@ukr.net>
2023-02-06 20:42:18 +0200

    six commit

--- Lab1_script.js
+++ Lab1_script.js
@@ -2,9 +2,8 @@ function myFunction(){
     let input = document.getElementById('input1');
     const output = document.getElementById('input1');
     
-
-        const requestURL = ('https://newton.vercel.app/api/v2/derive/' + input.value).replace('^', '%5E').replace('+', '%2B');
-
+    const requestURL = ('https://newton.vercel.app/api/v2/derive/' + input.value).replaceAll('^', '%5E').replaceAll('+', '%2B');
+    
     console.log(requestURL)
     const xhr = new XMLHttpRequest();
     xhr.open('GET',requestURL,true);
