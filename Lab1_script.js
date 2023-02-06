8da4bb5cf635b1c30d71a2f120a30f320f812b2a
FunkeDer <jafter@ukr.net>
2023-02-06 19:07:37 +0200

    first commit

---
+++ Lab1_script.js
@@ -0,0 +1,24 @@
+function myFunction(){
+    let input = document.getElementById('input1');
+    const output = document.getElementById('input1');
+    const requestURL = ('https://newton.vercel.app/api/v2/derive/' + input.value).replace(' ^ ', '%5E');
+    console.log(requestURL)
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET',requestURL,true);
+    xhr.send(null);
+    
+    xhr.onload = () => {
+        const answer = JSON.parse(xhr.response)
+        appendData (answer)
+    }
+   //console.log(xhr.response.operation)
+
+    function appendData(data) {
+        let output = document.getElementById('par');
+        let mainContainer = document.getElementById("output1");
+        output.innerHTML = 'Result: '  + data.result;
+    }
+    
+}
+
